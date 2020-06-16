6ed72e8c93f7d99ba1bf5a73a9c8a71076c69378
JBDCE <61785226+JBDCE@users.noreply.github.com>
2020-06-16 19:39:09 +0200

    Implemented Bait Fetch

--- sauce/me/JBDCE/uwuScammer/Main.java
+++ sauce/me/JBDCE/uwuScammer/Main.java
@@ -1,6 +1,18 @@
 package sauce.me.JBDCE.uwuScammer;
 
+import java.awt.Component;
+import java.awt.Graphics;
+import java.sql.Blob;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
+
+import sun.awt.image.BufferedImageDevice;
+
+import java.awt.image.*;
+import java.io.ByteArrayInputStream;
 
 public class Main {
 
@@ -10,7 +22,6 @@ public class Main {
         System.out.println("Hello World!");
         
         boolean succ = establishDBConnection();
-        
         if(succ){
             new InputWindow(fetchBaitImg(), fetchBaitTxt());
         }else{
@@ -19,15 +30,36 @@ public class Main {
     }
     
     private boolean establishDBConnection() {
-        return false;
+        try {
+            this.loiconn = DBConn.getInstance("localhost:3306/", "loli", "ahegao");
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
     }
     
     private String fetchBaitTxt() {
-        return null;
+        ResultSet rs = this.loiconn.executeQuery("SELECT text FROM t_bait ORDER BY RAND() LIMIT 1");
+        try {
+            rs.next();
+            return rs.getString("text");
+        } catch (SQLException e) {
+            return "Wait that is not Tennis...";
+        }
     }
     
-    private Icon fetchBaitImg() {
-        return null;
+    private BufferedImage fetchBaitImg() {
+        ResultSet rs = this.loiconn.executeQuery("SELECT dickpic FROM t_bait ORDER BY RAND() LIMIT 1");
+        BufferedImage bi = new BufferedImage(100,100, BufferedImage.TYPE_INT_RGB);
+        try {
+            rs.next();
+            Blob blob = rs.getBlob("dickpic");
+            byte[] dickbytes = blob.getBytes(1, (int)blob.length());
+            bi = ImageIO.read(new ByteArrayInputStream(dickbytes));
+        } catch (Exception e) {
+            //TODO Ein problem für zukunftsjbdce
+        }
+        return bi;
     }
     
     
