ff8426735ff7fa8a6a7c09253a1e96af418a3293
JBDCE <61785226+JBDCE@users.noreply.github.com>
2020-06-16 23:23:58 +0200

    Redistribution of exception handling and new db format

--- sauce/me/JBDCE/uwuScammer/Main.java
+++ sauce/me/JBDCE/uwuScammer/Main.java
@@ -1,29 +1,31 @@
 package sauce.me.JBDCE.uwuScammer;
 
-import java.awt.Component;
-import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.sql.Blob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
-
-import sun.awt.image.BufferedImageDevice;
-
-import java.awt.image.*;
-import java.io.ByteArrayInputStream;
 
 public class Main {
 
     private DBConn loiconn;
-    
+
     public Main(){
         System.out.println("Hello World!");
         
         boolean succ = establishDBConnection();
         if(succ){
-            new InputWindow(fetchBaitImg(), fetchBaitTxt());
+            ResultSet rs = fetchBait();
+            try {
+                rs.next();
+                new InputWindow(fetchBaitImg(rs), fetchBaitTxt(rs));
+                
+            } catch (Exception e) {
+                //TODO Ein problem für zukunftsjbdce2
+                
+            }
         }else{
             System.out.println("DBConn Failed.. Fix yo network.");
         }
@@ -31,34 +33,28 @@ public class Main {
     
     private boolean establishDBConnection() {
         try {
-            this.loiconn = DBConn.getInstance("localhost:3306/", "loli", "ahegao");
+            this.loiconn = DBConn.getInstance("localhost:3306/hhaven", "loli", "ahegao");
             return true;
         } catch (SQLException e) {
+            e.printStackTrace();
             return false;
         }
     }
-    
-    private String fetchBaitTxt() {
-        ResultSet rs = this.loiconn.executeQuery("SELECT text FROM t_bait ORDER BY RAND() LIMIT 1");
-        try {
-            rs.next();
-            return rs.getString("text");
-        } catch (SQLException e) {
-            return "Wait that is not Tennis...";
-        }
+
+    private ResultSet fetchBait(){
+        ResultSet rs = this.loiconn.executeQuery("SELECT text, dickpic FROM t_bait ORDER BY RAND() LIMIT 1");
+        return rs;
+    }
+
+    private String fetchBaitTxt(ResultSet rs) throws SQLException{
+        return rs.getString("text");
     }
     
-    private BufferedImage fetchBaitImg() {
-        ResultSet rs = this.loiconn.executeQuery("SELECT dickpic FROM t_bait ORDER BY RAND() LIMIT 1");
-        BufferedImage bi = new BufferedImage(100,100, BufferedImage.TYPE_INT_RGB);
-        try {
-            rs.next();
-            Blob blob = rs.getBlob("dickpic");
-            byte[] dickbytes = blob.getBytes(1, (int)blob.length());
-            bi = ImageIO.read(new ByteArrayInputStream(dickbytes));
-        } catch (Exception e) {
-            //TODO Ein problem für zukunftsjbdce
-        }
+    private BufferedImage fetchBaitImg(ResultSet rs) throws Exception{
+        BufferedImage bi = new BufferedImage(200,200, BufferedImage.TYPE_INT_RGB);
+        Blob blob = rs.getBlob("dickpic");
+        byte[] dickbytes = blob.getBytes(1, (int)blob.length());
+        bi = ImageIO.read(new ByteArrayInputStream(dickbytes));
         return bi;
     }
     
